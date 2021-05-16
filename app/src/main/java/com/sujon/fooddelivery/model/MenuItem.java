cf36ff0ab74ef12a1c458c397a60b832fc0db020
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-16 19:11:17 +0600

    9_Create Model Classes - Android Development with Java - Rakibul Islam - Bohubrihi
    9_Create Model Classes - Android Development with Java - Rakibul Islam - Bohubrihi

---
+++ app/src/main/java/com/sujon/fooddelivery/model/MenuItem.java
@@ -0,0 +1,34 @@
+package com.sujon.fooddelivery.model;
+
+public class MenuItem {
+    String itemName;
+    String itemDescription;
+    int itemPrice;
+
+    public MenuItem() {
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+}
