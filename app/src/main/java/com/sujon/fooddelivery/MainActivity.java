65a2c89418b571e4ffe263cc427344f03986d2bd
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-16 19:47:17 +0600

    10_upload data to firebase - Android Development with Java - Rakibul Islam - Bohubrihi
    10_upload data to firebase - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/fooddelivery/MainActivity.java
+++ app/src/main/java/com/sujon/fooddelivery/MainActivity.java
@@ -1,12 +1,21 @@
 package com.sujon.fooddelivery;
 
 import android.os.Bundle;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.sujon.fooddelivery.model.Restaurant;
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +32,31 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        //SendDataToFirestroe();
+
+    }
+
+    private void SendDataToFirestroe() {
+        FirebaseFirestore db= FirebaseFirestore.getInstance();
+        CollectionReference reference = db.collection("Restaurants");
+        Restaurant myRestaurant = new Restaurant();
+        myRestaurant.setRestaurantName("Sujon Restaurants");
+        myRestaurant.setRestaurantDescription("Best restaurant in Gaibandha");
+        myRestaurant.setRestaurantLocation("Bonarpara, Gaibandha");
+        myRestaurant.setRestaurantImageUrl("https://img.freepik.com/free-photo/assorted-indian-recipes-food-various_79295-7226.jpg?size=626&ext=jpg");
+
+        reference.add(myRestaurant).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<DocumentReference> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(MainActivity.this,"Restaurant Uploaded Successfully",Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(MainActivity.this,"Restaurant not Uploaded",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
